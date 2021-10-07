2e7dd1d6f9839e6383086733b7707e781c0d6099
Rohit Ghosal <rohit.ghosal1234@gmail.com>
2021-10-07 09:57:25 +0530

    Update bubble_sorting.java
    I have completed the code with some greater modified version. Hope you will make applaud with this.

--- bubble_sorting.java
+++ bubble_sorting.java
@@ -1,38 +1,68 @@
-// Java program for implementation of Bubble Sort
-class BubbleSort
+import java.util.*;
+
+public class bubble_sort
 {
-	void bubbleSort(int arr[])
+	public static void main(String[] args) throws Exception
 	{
-		int n = arr.length;
-		for (int i = 0; i < n-1; i++)
-			for (int j = 0; j < n-i-1; j++)
-				if (arr[j] > arr[j+1])
+		System.out.println("\t\t~~~Welcome toBingo_learnings!~~~\n\tThis is Bubble_sort in both fashion--by Ghosal_tuts ");
+		Scanner sc = new Scanner(System.in);char ch;
+		do
+		{
+			System.out.print("Enter the no of elements you want to sort: ");
+			int n = sc.nextInt();
+			System.out.println("Enter the elements-----");
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+			{
+				System.out.printf("Element[%d]= ",i+1);
+				arr[i]=sc.nextInt();
+			}
+			do
+			{
+				System.out.print("Enter in which type you want Ascending or Decending...?");
+				Scanner sp = new Scanner(System.in);
+				String c = sp.nextLine();
+				switch(c)
 				{
-					// swap arr[j+1] and arr[j]
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+				case "Ascending":
+					System.out.print("Ascending order output is: [ ");
+					for (int i = 0; i < n-1; i++)
+		            			for (int j = 0; j < n-i-1; j++)
+		                			if (arr[j] > arr[j+1])
+		                			{
+		                    			// swap temp and arr[i]
+		                    			int temp = arr[j];
+		                    			arr[j] = arr[j+1];
+		                    			arr[j+1] = temp;
+		                			}
+					 for (int i=0; i<n; ++i)
+		            		{	System.out.print(arr[i] + ", "); Thread.sleep(500);} //application of thread to stop function
+					System.out.println("]");
+					break;
+				case "Decending": 
+					System.out.print("decending order output is: [ ");
+					for (int i = 0; i < n-1; i++)
+		                                for (int j = 0; j < n-i-1; j++)
+		                                        if (arr[j] < arr[j+1])
+		                                        {
+		                                        // swap temp and arr[i]
+		                                        int temp = arr[j];
+		                                        arr[j] = arr[j+1];
+		                                        arr[j+1] = temp;
+		                                        }
+		                         for (int i=0; i<n; ++i)
+		                         {        System.out.print(arr[i] + ", "); Thread.sleep(500);}
+					System.out.println("]");
+					break;
+				default:
+					System.out.println("OoOw !you entered something another. Try again"); 
 				}
-	}
+				System.out.print("\nDo you want to continue with these elements(Type y or n) : ");
 
-	/* Prints the array */
-	void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i=0; i<n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-
-	// Driver method to test above
-	public static void main(String args[])
-	{
-		BubbleSort ob = new BubbleSort();
-		int arr[] = {64, 34, 25, 12, 22, 11, 90};
-		ob.bubbleSort(arr);
-		System.out.println("Sorted array");
-		ob.printArray(arr);
+	               ch = sc.next().charAt(0); 
+			}while (ch == 'Y'|| ch == 'y');
+			System.out.print("\nType A for sort with new elements or D for whole exit(Type A or D) : ");
+			ch = sc.next().charAt(0);
+		}while(ch == 'A' || ch == 'a');
 	}
 }
-/* This code is contributed by Rajat Mishra */
-
